6d8b435d04e10ddcf152aad3fa5a3fbae7848d0f
Faiyaz <mk3590653@gmail.com>
2026-02-20 19:17:56 +0600

    created a hovering animation in multiple images

---
+++ 4 projects with DOM/Multiple image hovering animation/script.js
@@ -0,0 +1,14 @@
+let elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseenter", function (dets) {
+    val.childNodes[1].style.opacity = 1;
+  });
+  val.addEventListener("mouseleave", function (dets) {
+    val.childNodes[1].style.opacity = 0;
+  });
+  val.addEventListener("mousemove", function (dets) {
+    val.childNodes[1].style.left = dets.x+'px';
+    val.childNodes[1].style.top = dets.y+'px';
+  });
+});
