4cf9e202af2bd9d7087e5af821657039ff266fd9
Faiyaz <mk3590653@gmail.com>
2026-04-05 21:10:16 +0600

    advanced js

---
+++ Day59/script.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    introduce(){
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old`);
+    }
+}
+let person1 = new Person("Ahmad", 25);
+person1.introduce();
